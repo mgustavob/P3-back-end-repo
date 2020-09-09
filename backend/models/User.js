0ace2dc8cad6c5870aa952f8f85db549c6113cda
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-09-09 12:33:28 -0700

    [games] Add and test some CRUD with Lizz

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,23 +18,7 @@ const userSchema = new Schema({
   favedGames: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Game'
-  }],
-  linkedIn: {
-    type: String,
-    default: 'Anoymous'
-  },
-  github: {
-    type: String,
-    default: 'Anonymous'
-  },
-  profilePic: {
-    type: String,
-    default: 'no photo available'
-  },
-  portfolioUrl: {
-    type: String,
-    default: 'Anonymous'
-  }
+  }]
 });
 
 module.exports = mongoose.model('User', userSchema);
